37becd82ddf5c250a3a4198065dd5bd40aed24c6
chenitobuns <chen.kevin620@gmail.com>
2016-07-19 22:38:32 -0700

    Daily Milestone Success

--- UPanda_Official/UPanda_Official/ViewController.swift
+++ UPanda_Official/UPanda_Official/ViewController.swift
@@ -14,8 +14,8 @@ import FBSDKLoginKit
 class ViewController: UIViewController {
 
     
-    //@IBOutlet weak var emailField: UITextField!
-    //@IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     
     
     override func viewDidLoad() {
